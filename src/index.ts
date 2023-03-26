40f399f02ad1569dc16ad438b824bd58b38d911a
ducktrshessami <jwee761@gmail.com>
2023-03-26 16:22:52 -0700

    use timestamp header for relative live timestamp

--- src/index.ts
+++ src/index.ts
@@ -43,13 +43,18 @@ export default {
             if (!(await verifyRequest(env.TWITCH_SECRET, request, body))) {
                 return new Response(null, { status: 401 });
             }
-            checkAge(request, env);
+            const timestamp = resolveTimestamp(request, env);
             const json: WebhookBody = JSON.parse(await body.text());
             if (!isStreamOnlineBody(json)) {
                 return new Response(null, { status: 403 });
             }
             switch (request.headers.get(RequestHeaders.MessageType)) {
-                case NotificationType.Notification: return handleNotification(env, ctx, <StreamOnlineNotificationBody>json);
+                case NotificationType.Notification: return handleNotification(
+                    env,
+                    ctx,
+                    <StreamOnlineNotificationBody>json,
+                    timestamp
+                );
                 case NotificationType.WebhookCallbackVerification: return handleChallenge(<StreamOnlineCallbackVerificationBody>json);
                 case NotificationType.Revocation: return handleRevocation(env, ctx, json);
             }
@@ -61,19 +66,25 @@ export default {
     },
 };
 
-function checkAge(request: Request, env: Env): void {
+function checkAge(
+    env: Env,
+    timestamp: number,
+    rawTimestamp: string
+): void {
     const TWITCH_AGE_WARNING = env.TWITCH_AGE_WARNING ? parseInt(env.TWITCH_AGE_WARNING) : 300000;
     const now = new Date();
-    const rawTimestamp = requestHeader(request, RequestHeaders.MessageTimestamp);
-    const timestamp = new Date(rawTimestamp);
-    const age = now.getTime() - timestamp.getTime();
+    const age = now.getTime() - timestamp;
     if (age > TWITCH_AGE_WARNING) {
         console.warn(`[${now.toISOString()}] Received request with timestamp older than ${TWITCH_AGE_WARNING} ms: '${rawTimestamp}'`);
         // TODO: Age warning
     }
 }
 
-async function forwardNotification(env: Env, body: StreamOnlineNotificationBody): Promise<void> {
+async function forwardNotification(
+    env: Env,
+    body: StreamOnlineNotificationBody,
+    timestamp: number
+): Promise<void> {
     try {
         const info = await fetchEventSubStreamInfo(env, body);
         await executeWebhook(resolveWebhookEnvs(env), {
@@ -84,7 +95,7 @@ async function forwardNotification(env: Env, body: StreamOnlineNotificationBody)
                 title: info.title,
                 url: channelUrl(info.userLogin),
                 thumbnail: { url: info.userAvatarUrl },
-                description: `${info.userName} ${info.startedAt ? "went live" : "goes live"} ${relativeTimestamp(info.startedAt?.getTime() ?? Date.now() + info.delay)}\nPlaying ${info.gameName}`,
+                description: `${info.userName} ${info.startedAt ? "went live" : "goes live"} ${relativeTimestamp(info.startedAt?.getTime() ?? timestamp + info.delay)}\nPlaying ${info.gameName}`,
                 image: info.thumbnailUrl ? { url: info.thumbnailUrl } : undefined
             }]
         });
@@ -111,8 +122,32 @@ async function forwardRevocation(env: Env, body: StreamOnlineRevocationBody): Pr
     }
 }
 
-function handleNotification(env: Env, ctx: ExecutionContext, body: StreamOnlineNotificationBody): Response {
-    ctx.waitUntil(forwardNotification(env, body));
+function resolveTimestamp(request: Request, env: Env): number {
+    const rawTimestamp = requestHeader(request, RequestHeaders.MessageTimestamp);
+    if (rawTimestamp) {
+        const parsedTimestamp = new Date(rawTimestamp);
+        const timestamp = parsedTimestamp.getTime();
+        checkAge(env, timestamp, rawTimestamp);
+        return timestamp;
+    }
+    else {
+        return Date.now();
+    }
+}
+
+function handleNotification(
+    env: Env,
+    ctx: ExecutionContext,
+    body: StreamOnlineNotificationBody,
+    timestamp: number
+): Response {
+    ctx.waitUntil(
+        forwardNotification(
+            env,
+            body,
+            timestamp
+        )
+    );
     return new Response(null, { status: 204 });
 }
 
